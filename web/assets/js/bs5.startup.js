9d9ae3a6f572f4735e57d181393fc039214d8903
wasori <wasori@naver.com>
2024-07-02 15:50:40 +0900

    bs5.startup.js 수정

--- web/assets/js/bs5.startup.js
+++ web/assets/js/bs5.startup.js
@@ -108,11 +108,11 @@ function showLoginNotices(){
     $.each([
         !$user.details.sub ? {
             isValid: !$user.details.size || parseInt($user.details.size) < 20000,
-            PNotify: {
-                type:'warning',
-                title: lang['Max Storage Amount'],
-                text: lang.setMaxStorageAmountText,
-            }
+            // PNotify: {
+            //     type:'warning',
+            //     title: lang['Max Storage Amount'],
+            //     text: lang.setMaxStorageAmountText,
+            // }
         } : {}
     ],function(n,notice){
         if(notice.isValid){
